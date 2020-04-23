3bdc155af7be8cdbcc8d895a91870398667661e5
Purusothaman Ramanujam <purushoth.r@gmail.com>
2020-04-23 19:52:38 +0530

    New Changes

--- android/src/main/java/in/mertcan/advancedshare/shareintents/Base.java
+++ android/src/main/java/in/mertcan/advancedshare/shareintents/Base.java
@@ -47,15 +47,15 @@ public abstract class Base {
 
         if (checkKey("url")) {
             if (fileHelper.isFile()) {
+                List<ResolveInfo> resInfoList = registrar.context().getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    registrar.context().grantUriPermission(packageName, fileHelper.getUri(), Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+
                 intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.putExtra(Intent.EXTRA_STREAM, fileHelper.getUri());
                 intent.setType(fileHelper.getType());
-
-                List<ResolveInfo> resInfoList = this.registrar.activity().getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-                for (ResolveInfo resolveInfo : resInfoList) {
-                    String packageName = resolveInfo.activityInfo.packageName;
-                    this.registrar.activity().grantUriPermission(packageName, fileHelper.getUri(), Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                }
             }
         }
         return 0;
